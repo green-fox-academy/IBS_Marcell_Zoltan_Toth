473692497c2339f66167b36922b172e139ea65ba
guthixx23 <guthixx23@gmail.com>
2020-11-19 18:11:09 +0100

    Add/checking/removeing multiple elements and the corresponding error handling has been implemented

--- week-06/TODOapp/todo_application.py
+++ week-06/TODOapp/todo_application.py
@@ -45,16 +45,45 @@ def add_new_task(new_task):
     tasks_global.append("[ ] " + new_task)
 
 
-def remove_task(index):
+def remove_task(indexes):
     global tasks_global
-    tasks_global.pop(index - 1)
+    indexes.sort(reverse=True)
+    for i in indexes:
+        tasks_global.pop(i - 1)
 
 
-def check_task(index):
+def check_task(indexes):
     global tasks_global
-    tasks_global[index - 1] = "[x" + tasks_global[index - 1][2:]
+    indexes.sort()
+    for i in indexes:
+        tasks_global[i - 1] = "[x" + tasks_global[i - 1][2:]
 
 
+def argument_ok(arg, case):
+    if not sys.argv[2].isnumeric():
+        print("Unable to " + case + ": index is not a number")
+        return False
+    elif len(tasks_global) < int(arg):
+        print("Unable to " + case + ": index is out of bound")
+        return False
+    else:
+        return True
+
+
+def sort_arguments(case):
+    ret = []
+    good_args = []
+    bad_args = []
+
+    for i in sys.argv[2:]:
+        if argument_ok(int(i), case):
+            good_args.append(int(i))
+        else:
+            bad_args.append(i)
+
+    ret.append(good_args)
+    ret.append(bad_args)
+    return ret
 
 
 
@@ -70,9 +99,10 @@ elif sys.argv[1] == "-l" or sys.argv[1] == "-List":
     print_data_unchecked(tasks_global)
 
 elif sys.argv[1] == "-a" or sys.argv[1] == "-Add":
-    if len(sys.argv) == 3:
+    if len(sys.argv) > 2:
         tasks_global = load_data()
-        add_new_task(sys.argv[2])
+        for i in range(2,len(sys.argv)):
+            add_new_task(sys.argv[i])
         save_data(tasks_global)
     else:
         print("Unable to add: no task provided")
@@ -80,34 +110,26 @@ elif sys.argv[1] == "-a" or sys.argv[1] == "-Add":
 elif sys.argv[1] == "-r" or sys.argv[1] == "-Remove":
 
     tasks_global = load_data()
+    args_in_order = sort_arguments("remove")
 
-    if len(sys.argv) == 2:
-        print("Unable to remove: no index provided")
-    else:
-        if sys.argv[2].isnumeric():
-            if len(tasks_global) < int(sys.argv[2]):
-                print("Unable to remove: index is out of bound")
-            else:
-                remove_task(int(sys.argv[2]))
-                save_data(tasks_global)
-        else:
-            print("Unable to remove: index is not a number")
+    if len(args_in_order[0]) != 0:
+        remove_task(args_in_order[0])
+        save_data(tasks_global)
 
+    if len(args_in_order[1]) != 0:
+        print("The following argument(s) are invalid: " + str(args_in_order[1]))
 
 elif sys.argv[1] == "-c" or sys.argv[1] == "-Check":
-    tasks_global = load_data()
 
-    if len(sys.argv) == 2:
-        print("Unable to check: no index provided")
-    else:
-        if sys.argv[2].isnumeric():
-            if len(tasks_global) < int(sys.argv[2]):
-                print("Unable to check: index is out of bound")
-            else:
-                check_task(int(sys.argv[2]))
-                save_data(tasks_global)
-        else:
-            print("Unable to check: index is not a number")
+    tasks_global = load_data()
+    args_in_order = sort_arguments("check")
+
+    if len(args_in_order[0]) != 0:
+        check_task(args_in_order[0])
+        save_data(tasks_global)
+
+    if len(args_in_order[1]) != 0:
+        print("The following argument(s) are invalid: " + str(args_in_order[1]))
 
 else:
     print("Unsupported argument")
